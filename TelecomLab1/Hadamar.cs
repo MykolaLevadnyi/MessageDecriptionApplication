64dc26d7d8803893ef21f062d1962d2f5b13f252
Mykola Levanyi <levadnik12@gmail.com>
2022-04-11 23:10:06 +0300

    main

---
+++ TelecomLab1/Hadamar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TelecomLab1
+{
+	public static class Hadamard
+	{
+		public static List<List<int>> generateHadamardMatrix(int size)
+		{
+			// Сheck if size is power of 2
+			if(!IsPowerOfTwo(size)) {
+				throw new Exception("Wrong size for Hadamard Matrix!");
+			}
+
+			// Matrix initialization
+			List<List<int>> hadamarMatrix = new List<List<int>>();
+			for (int i = 0; i < size; i++) {
+				hadamarMatrix.Add(new List<int>());
+				for (int j = 0; j < size; j++) {
+					hadamarMatrix[i].Add(0);
+				}
+			}
+
+			hadamarMatrix[0][0] = 1;
+			for (int i = 1; i < size; i += i) {
+				for (int j = 0; j < i; j++) {
+					for (int k = 0; k < i; k++) {
+						hadamarMatrix[j + i][k] = hadamarMatrix[j][k];
+						hadamarMatrix[j][k + i] = hadamarMatrix[j][k];
+						hadamarMatrix[j + i][k + i] = -hadamarMatrix[j][k];
+					}
+				}
+			}	
+			return hadamarMatrix;
+		}
+
+		private static bool IsPowerOfTwo(int number)
+		{
+			for (int x = 1; x <= number; x *= 2) {
+				if (x == number) return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ TelecomLab1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TelecomLab1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try {
+				int wCodeIndex = 13;
+				int hadamarMatrixSize = 16;
+
+				//Hadamard matrix generation
+				var hadamardMatrix = Hadamard.generateHadamardMatrix(hadamarMatrixSize);
+				Console.WriteLine("Hadamard matrix:\n" +
+					string.Join("\n", hadamardMatrix.Select(line =>
+						string.Join(", ", line.Select(number => string.Format("{0,2}", number)))))
+					+ "\n");
+
+				List<int> WCode = hadamardMatrix[wCodeIndex];
+				Console.WriteLine($"Warshall code: ({string.Join(", ", WCode)})\n");
+
+				// Read encrypted sequence from file
+				StreamReader objInput = new StreamReader("DataBinary.dat", System.Text.Encoding.Default);
+				string contents = objInput.ReadToEnd().Trim();
+				string[] split = Regex.Split(contents, "\\s+", RegexOptions.None);
+				List<int> EncryptedSequence = split.Select(n => int.Parse(n)).ToList();
+
+				if (EncryptedSequence.Count % WCode.Count != 0) {
+					throw new Exception("Encrypted sequence have wrong size!");
+
+				}
+
+				Console.WriteLine($"Encrypted sequence: ({string.Join(", ", EncryptedSequence)})\n");
+
+				// Split Encrypted sequence
+				List<List<int>> EncryptedSequenceParts = EncryptedSequence.ChunkBy(WCode.Count).Select(p => p.ToList()).ToList();
+
+				List<int> CC = new List<int>();
+				// Count correlations
+				for (int i = 0; i < EncryptedSequenceParts.Count; i++) {
+					var encryptedSequencePart = EncryptedSequenceParts[i];
+					var CC_i = encryptedSequencePart.Select((value, index) => value * WCode.ElementAt(index)).Sum() / hadamarMatrixSize;
+					CC.Add(CC_i);
+
+					Console.WriteLine($"CC{i}: {CC_i}");
+				}
+				Console.WriteLine();
+
+				// Rebase to binary
+				List<int> CCBinary = CC.Select(v => v == 1 ? 0 : 1).ToList();
+
+				Console.WriteLine($"Decrypted binary sequence: ({string.Join(", ", CCBinary)})\n");
+			} catch (Exception ex) {
+				Console.WriteLine(ex.Message);
+			}
+		}
+	}
+}
